cc0eda13bc9cff4c6d94ea84b5a30fcb06d17772
nimigeanconstantinion <constantin.nimigean@gmail.com>
2024-03-08 10:21:25 +0200

    commit 08032024

--- src/main/java/com/example/commandservice/web/ComController.java
+++ src/main/java/com/example/commandservice/web/ComController.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/command")
-@CrossOrigin
+//@CrossOrigin
 public class ComController {
 
     private MapStocService mapStocService;
